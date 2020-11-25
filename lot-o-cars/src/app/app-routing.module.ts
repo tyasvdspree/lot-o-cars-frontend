b28532e4fa42afee5d5d23e5ba4341881c48b09e
Mike van Niel <0152110@hr.nl>
2020-11-25 22:02:56 +0100

    Integrated components and added routing

--- lot-o-cars/src/app/app-routing.module.ts
+++ lot-o-cars/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ContactComponent } from './components/contact/contact.component';
+import { LandingspageComponent } from './components/landingspage/landingspage.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: LandingspageComponent },
+  { path: 'contact', component: ContactComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes, { relativeLinkResolution: 'legacy' })],
