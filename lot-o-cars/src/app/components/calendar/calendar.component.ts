cc1f3a1a96e70de13b0d533b50bbcf316bdbdb2c
Mike van Niel <0152110@hr.nl>
2020-12-11 21:56:49 +0100

    Added mindate to calendar so blocked dates can be limited

--- lot-o-cars/src/app/components/calendar/calendar.component.ts
+++ lot-o-cars/src/app/components/calendar/calendar.component.ts
@@ -6,20 +6,30 @@ import { Component, Input, OnInit } from '@angular/core';
   styleUrls: ['./calendar.component.scss']
 })
 export class CalendarComponent implements OnInit {
+  minDate: Date;
   selectedDate: Date;
   @Input() blockedDates: Date[];
 
   constructor() { }
 
   ngOnInit(): void {
+    this.setMinCalendarDate();
   }
 
   dateChanged(date): void {
-    alert(`Selected: ${date}`);
+    console.log(`Selected: ${date}`);
   }
 
+  // this function returns false if a date param d must be blocked on the calendar.
+  // date d is checked for the array of blocked dates and if it is found
+  // (indexOf != -1) it wil return false;
   myDateFilter = (d: Date): boolean => {
     return this.blockedDates.map(Number).indexOf(+d) === -1;
+  }
+
+  setMinCalendarDate(): void {
+    const today = new Date();
+    this.minDate = new Date(today.getFullYear(), today.getMonth(), 1);
   }
 
 }
