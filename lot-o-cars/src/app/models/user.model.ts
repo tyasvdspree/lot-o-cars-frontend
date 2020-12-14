e5d9df0b0f0fd1ee66d4b621bde1c547e51b7e71
Volkan <volkanbozdogan84@gmail.com>
2020-12-14 19:48:40 +0100

    added fields to registration

--- lot-o-cars/src/app/models/user.model.ts
+++ lot-o-cars/src/app/models/user.model.ts
@@ -4,10 +4,18 @@ export class User {
 
     constructor(
         username: string,
-        password: string
+        password: string,
+        firstname: string,
+        lastname: string,
+        phonenumber: string,
+        emailaddress: string,
     ){
         this.username = username;
         this.password = password;
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.phonenumber = phonenumber;
+        this.emailaddress = emailaddress;
     }
 
     id: number;
