6bfad8a10edf66b0284deb32cfd8bf0ce62323f3
Jordy <0934433@hr.nl>
2021-01-09 21:54:54 +0100

    typo

--- lot-o-cars/src/app/enums/fuel.enum.ts
+++ lot-o-cars/src/app/enums/fuel.enum.ts
@@ -1,6 +1,6 @@
 export enum Fuel {
     PATROL = "Benzine",
-    DIEZEL = "Diezel",
+    DIESEL = "Diesel",
     GAS = "Gas"
 }
 export interface FuelSpec {
@@ -11,8 +11,8 @@ export function getFuelSpec(fuel: Fuel):FuelSpec {
     switch(fuel){
         case Fuel.PATROL:
             return {/*TODO: translation function for display value*/displayText: "Benzine", value: 0}
-        case Fuel.DIEZEL:
-            return {/*TODO: translation function for display value*/displayText: "Diezel", value: 1}
+        case Fuel.DIESEL:
+            return {/*TODO: translation function for display value*/displayText: "Diesel", value: 1}
         case Fuel.GAS:
             return {/*TODO: translation function for display value*/displayText: "Gas", value: 2}
     }
