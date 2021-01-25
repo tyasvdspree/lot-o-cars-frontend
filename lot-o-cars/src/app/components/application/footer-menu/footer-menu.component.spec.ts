5e1e74c6f5e4a8ab6271bb0564818692e47fd9e0
Mike van Niel <0152110@hr.nl>
2021-01-25 10:44:09 +0100

    Added footermenu tests

--- lot-o-cars/src/app/components/application/footer-menu/footer-menu.component.spec.ts
+++ lot-o-cars/src/app/components/application/footer-menu/footer-menu.component.spec.ts
@@ -1,6 +1,9 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { FootermenuComponent } from './footer-menu.component';
+import { By } from '@angular/platform-browser';
+import { RouterTestingModule } from '@angular/router/testing';
+import { Component } from '@angular/core';
+import {Location} from '@angular/common';
 
 describe('FootermenuComponent', () => {
   let component: FootermenuComponent;
@@ -8,7 +11,17 @@ describe('FootermenuComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ FootermenuComponent ]
+      declarations: [ 
+        FootermenuComponent 
+      ],
+      imports: [
+        RouterTestingModule.withRoutes(
+          [
+            { path: 'contact', component: DummyComponent },
+            { path: 'terms', component: DummyComponent }
+          ]
+        )
+      ]
     })
     .compileComponents();
   });
@@ -22,4 +35,49 @@ describe('FootermenuComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should have an span element with text: 2021 (c) lot-o-cars', () => {
+    const elem = fixture.debugElement.query(By.css('span'));
+    expect(elem.nativeElement.textContent.trim()).toBe('2021 © lot-o-cars');
+  });
+
+  it('should have two anchor elements', () => {
+    const elems = fixture.debugElement.queryAll(By.css('a'));
+    expect(elems.length).toBe(2);
+  });
+
+  it('should have an anchor element with text: contact', () => {
+    const elems = fixture.debugElement.queryAll(By.css('a'));
+    expect(elems[0].nativeElement.textContent.trim()).toBe('contact');
+  });
+
+  it('should have an anchor element with text: voorwaarden', () => {
+    const elems = fixture.debugElement.queryAll(By.css('a'));
+    expect(elems[1].nativeElement.textContent.trim()).toBe('voorwaarden');
+  });
+
+  it('should navigate to /contact on anchor contact click', () => {
+    const location = TestBed.get(Location);
+    const links = fixture.debugElement.queryAll(By.css('a'));
+    const contactAnchor = links[0].nativeElement;
+    contactAnchor.click();
+    fixture.detectChanges();
+    fixture.whenStable().then(() => {
+      expect(location.path()).toBe('/contact');
+    });
+  });
+
+  it('should navigate to /terms on anchor voorwaarden click', () => {
+    const location = TestBed.get(Location);
+    const links = fixture.debugElement.queryAll(By.css('a'));
+    const termsAnchor = links[1].nativeElement;
+    termsAnchor.click();
+    fixture.detectChanges();
+    fixture.whenStable().then(() => {
+      expect(location.path()).toBe('/terms');
+    });
+  });
 });
+
+@Component({template: ''})
+class DummyComponent {}
