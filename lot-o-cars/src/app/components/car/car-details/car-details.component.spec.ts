57d07df4c1d8cd088f471933590f21ea4d12bdc3
Mike van Niel <0152110@hr.nl>
2021-01-25 12:11:43 +0100

    Added tests for car details

--- lot-o-cars/src/app/components/car/car-details/car-details.component.spec.ts
+++ lot-o-cars/src/app/components/car/car-details/car-details.component.spec.ts
@@ -1,5 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { By } from '@angular/platform-browser';
+import { Car } from 'src/app/models/car.model';
+import { Location } from 'src/app/models/location.model';
+import { User } from 'src/app/models/user.model';
 import { CardetailsComponent } from './car-details.component';
 
 describe('CardetailsComponent', () => {
@@ -22,4 +25,50 @@ describe('CardetailsComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should have 1 anchor element before a car was loaded', () => {
+    const elems = fixture.debugElement.queryAll(By.css('a'));
+    expect(elems.length).toBe(1);
+  });
+
+  it('should have 2 anchor elements after a car was loaded', () => {
+    const dummyCar = new Car();
+    dummyCar.location = new Location();
+    dummyCar.user = new User('', '', '', '', '','');
+    component.car = dummyCar;
+    fixture.detectChanges();
+    const elems = fixture.debugElement.queryAll(By.css('a'));
+    expect(elems.length).toBe(2);
+  });
+
+  it('should have 5 mat-card elements', () => {
+    const elems = fixture.debugElement.queryAll(By.css('mat-card'));
+    expect(elems.length).toBe(5);
+  });
+
+  it('should have a first mat-card element with title foto\'s', () => {
+    const elems = fixture.debugElement.queryAll(By.css('mat-card-title'));
+    expect(elems[0].nativeElement.textContent.trim()).toBe('foto\'s');
+  });
+
+  it('should have a second mat-card element with title auto', () => {
+    const elems = fixture.debugElement.queryAll(By.css('mat-card-title'));
+    expect(elems[1].nativeElement.textContent.trim()).toBe('auto');
+  });
+
+  it('should have a third mat-card element with title ophaaladres', () => {
+    const elems = fixture.debugElement.queryAll(By.css('mat-card-title'));
+    expect(elems[2].nativeElement.textContent.trim()).toBe('ophaaladres');
+  });
+
+  it('should have a fourth mat-card element with title verhuurder', () => {
+    const elems = fixture.debugElement.queryAll(By.css('mat-card-title'));
+    expect(elems[3].nativeElement.textContent.trim()).toBe('verhuurder');
+  });
+
+  it('should have a fifth mat-card element with title beschikbaarheid', () => {
+    const elems = fixture.debugElement.queryAll(By.css('mat-card-title'));
+    expect(elems[4].nativeElement.textContent.trim()).toBe('beschikbaarheid');
+  });
+
 });
